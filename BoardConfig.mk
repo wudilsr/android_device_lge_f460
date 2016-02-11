4690c4f9f4e43931f660a5d5b0cb2d7e1874b279
shminer <a332574643@gmail.com>
2016-02-12 00:10:36 +0800

    tiger6: enable cpuset

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI  := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := krait
+ENABLE_CPUSETS := true
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := APQ8084
