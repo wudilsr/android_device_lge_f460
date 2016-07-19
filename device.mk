f82e96d51740b6efdf34fc5e161bb779ac9e66e5
shminer <a332574643@gmail.com>
2016-07-19 08:15:07 +0800

    Tiger6: Fix signal display

--- device.mk
+++ device.mk
@@ -160,6 +160,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+# rssi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/rssi.xml:system/etc/rssi.xml
     
 # OMX
 PRODUCT_PACKAGES += \
