056f97de435f59064a0ccbafd66977b208eacc6a
wudilsr <wudilsr@gmail.com>
2021-10-17 18:48:25 +0800

    f460: Add COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,9 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/full_f460.mk
+	$(LOCAL_DIR)/full_f460.mk \
+	$(LOCAL_DIR)/lineage.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_f460-user \
+    lineage_f460-userdebug \
+    lineage_f460-eng	
+	
