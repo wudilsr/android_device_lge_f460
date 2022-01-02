388a936dd4e659eeca7d827f31e330e119ab7a58
wudilsr <wudilsr@gmail.com>
2022-01-02 20:04:03 +0800

    f460: Add seccomp policy

--- device.mk
+++ device.mk
@@ -315,7 +315,12 @@ PRODUCT_BOOT_JARS += \
 # Radio
 PRODUCT_PACKAGES += \
     libxml2
-    
+
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+        
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
