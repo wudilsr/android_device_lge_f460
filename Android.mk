b7ae6df7e0940d18193e8a53de8caad4871b5a45
wudilsr <wudilsr@gmail.com>
2022-01-19 20:14:09 +0800

    f460: Fix keymaster firmware links

--- Android.mk
+++ Android.mk
@@ -73,14 +73,14 @@ $(DXHDCP2_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 ALL_DEFAULT_INSTALLED_MODULES += $(DXHDCP2_SYMLINKS)
 
 KEYMASTER_IMAGES := \
-    keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaste.mdt
+    keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaster.mdt
 
-KEYMASTER_SYMLINKS :=$(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(KEYMASTER_IMAGES))
+KEYMASTER_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/keymaster/,$(notdir $(KEYMASTER_IMAGES)))
 $(KEYMASTER_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "keymaster firmware link: $@"
+	@echo "Keymaster firmware link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+	$(hide) ln -sf /firmware/image/`echo $(notdir $@) | sed 's/r\././'` $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(KEYMASTER_SYMLINKS)
 
